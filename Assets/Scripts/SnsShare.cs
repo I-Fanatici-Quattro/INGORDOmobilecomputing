dc36161f54508801d78771b00baee0cfaaf4a0c0
Alessio <a.like.p0@gmail.com>
2021-07-21 15:54:46 +0200

    Share 1.0
    Prima versione per la condivisione dei risultati

---
+++ Assets/Scripts/SnsShare.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SnsShare : MonoBehaviour
+{
+
+	public void Share()
+    {
+		StartCoroutine(TakeScreenshotAndShare());
+    }
+
+	private IEnumerator TakeScreenshotAndShare()
+	{
+		yield return new WaitForEndOfFrame();
+
+		Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+		ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+		ss.Apply();
+
+		string filePath = System.IO.Path.Combine(Application.temporaryCachePath, "share.png");
+		System.IO.File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+		Destroy(ss);
+
+		new NativeShare().AddFile(filePath)
+			.SetSubject("").SetText("").SetUrl("")
+			.SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+			.Share();
+	}
+
+	}
